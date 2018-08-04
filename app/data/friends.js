ba4b0decccc857bc29973b0ac47409bd724062c8
stefaniiik <stefanikrautstrunk@gmail.com>
2018-08-03 17:18:46 -0700

    project

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,22 @@
 
+  var friendList = [
+    {
+      name: 'Starlord',
+      profilePic:'https://goo.gl/images/j7sNoJ',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
   
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,22 @@
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+// path package to get the correct file path for our html
+
+var path = require('path');
+
+module.exports = function (app) {
+
+	// HTML GET Requests
+	app.get('/survey', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
